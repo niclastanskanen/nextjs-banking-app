e6da1af0a1365aa0db83eb3d473288f60b46efde
Niclas <niclastanskanen@hotmail.com>
2024-08-11 17:12:39 +0200

    feat: Add header and total balance box to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,25 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Niclas' }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage tour account and transactions efficeintly'
+          />
+
+          <TotalBalanceBox />
+        </header>
+      </div>
+    </section>
   )
 }
 
