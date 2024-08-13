b94ebb0291c7a2b9058a73a4c84b7617d41bf1b5
Niclas <niclastanskanen@hotmail.com>
2024-08-13 11:04:50 +0200

    refactor: Update CustomInput component to use updated formSchema

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -6,9 +6,11 @@ import { Input } from '@/components/ui/input'
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
+const formSchema = authFormSchema('sign-up')
+
 interface CustomInput {
-  control: Control<z.infer<typeof authFormSchema>>
-  name: FieldPath<z.infer<typeof authFormSchema>>
+  control: Control<z.infer<typeof formSchema>>
+  name: FieldPath<z.infer<typeof formSchema>>
   label: string
   placeholder: string
 }
