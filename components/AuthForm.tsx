80ad595a04ad5b551edcf4a19877e582365fd5fc
Niclas <niclastanskanen@hotmail.com>
2024-08-15 09:58:29 +0200

    feat: Update AuthForm to include additional user data
    The code changes in this commit update the AuthForm component to include additional user data fields such as first name, last name, address, city, state, postal code, date of birth, social security number, email, and password. This enhancement allows for more comprehensive user registration and improves the overall user experience.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -37,7 +37,20 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
       // Sign up with Appwrite & create plain link token
       if(type === 'sign-up') {
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData)
 
         setUser(newUser)
       }
