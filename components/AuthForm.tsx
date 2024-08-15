618e7abdff0b1c7649776402665fb35e1028d1bb
Niclas <niclastanskanen@hotmail.com>
2024-08-15 10:47:30 +0200

    feat: Update AuthForm to include user data fields
    The code changes in this commit update the AuthForm component to include additional user data fields such as first name, last name, address, city, state, postal code, date of birth, social security number, email, and password. This enhancement allows for more comprehensive user registration and improves the overall user experience.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -101,11 +101,11 @@ const AuthForm = ({ type }: { type: string }) => {
           </h1>
         </div>
       </header>
-      {/* {user ? ( */}
+      {user ? (
         <div className='flex flex-col gap-4'>
           <PlaidLink user={user} variant='primary' />
         </div>
-      {/* ) : ( */}
+        ) : (
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -213,7 +213,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </Link>
           </footer>
         </>
-      {/* )} */}
+      )}
     </section>
   )
 }
