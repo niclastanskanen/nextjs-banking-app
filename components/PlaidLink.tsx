46efdfd29086a850488993cc0442867cacd1a85d
Niclas <niclastanskanen@hotmail.com>
2024-08-15 10:47:34 +0200

    feat: Update PlaidLink component to exchange public token for access token
    The code changes in this commit update the PlaidLink component to include the functionality to exchange the public token obtained from Plaid for an access token and item ID. This enhancement improves the bank account linking process and enhances the overall user experience.

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -3,7 +3,7 @@ import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-
 import { useRouter } from 'next/navigation'
 
 import { Button } from './ui/button'
-import { createLinkToken } from '@/lib/actions/user.actions'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
 
 const PlaidLink = ({ user, variant}: PlaidLinkProps) => {
   const router = useRouter()
@@ -20,10 +20,10 @@ const PlaidLink = ({ user, variant}: PlaidLinkProps) => {
   }, [user])
 
   const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
-    // await exchangePublicToken({
-    //   publicToken: public_token,
-    //   user,
-    // })
+    await exchangePublicToken({
+      publicToken: public_token,
+      user,
+    })
     router.push('/')
   }, [user])
 
