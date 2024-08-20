c09dbddd337e5ee5f9d367f4cd0f4886810fd1bf
Niclas <niclastanskanen@hotmail.com>
2024-08-20 12:45:35 +0200

    feat: Add TransactionsTable component for displaying transaction data
    This commit adds the TransactionsTable component, which is responsible for displaying transaction data. It includes the necessary imports and logic to handle the display of transactions for a specific bank account. This component enhances the user experience by providing a comprehensive view of their transaction history.

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from './BankTabItem'
 import BankInfo from './BankInfo'
+import TransactionsTable from './TransactionsTable'
 
 
 const RecentTransactions = ({
@@ -49,6 +50,7 @@ const RecentTransactions = ({
               appwriteItemId={appwriteItemId}
               type="full"
             />
+            <TransactionsTable transactions={transactions} />
           </TabsContent>
         ))}
       </Tabs>
